5791fa89f366208e934c17971b5c3e40df2e303a
Hunter King <hking6776@gmail.com>
2023-08-14 15:36:56 -0500

    cleantree

--- src/TokenPair.sol
+++ src/TokenPair.sol
@@ -1,18 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
+import {ReentracyGuard} from "@openzeppelin/security/ReentracyGuard.sol";
+import {SafeERC20} from "@openzeppelin/token/ERC20/utils/SafeERC20.sol";
+import {IERC20} from "@openzeppelin/token/ERC20/IERC20.sol";
+
 contract TokenPair {
-    uint256 locked;
+    using SafeERC20 for IERC20;
 
     address public t0;
     address public t1;
 
-    modifier lock() {
-        require(locked == 0, "Lock on");
-        locked = 1;
-        _;
-        locked = 0;
+    function initialize(address _t0, address _t1) external {
+        require(t0 == address(0), "Initialized");
+        t0 = _t0;
+        t1 = _t1;
     }
-
-    function initialize(address t0, address t1) external {}
 }
